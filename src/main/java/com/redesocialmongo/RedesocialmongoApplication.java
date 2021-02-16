9cef43488c4ad79b90712a5b14c075736dfc0c21
Hugo Nascimento <hugonsantos22@gmail.com>
2021-02-16 16:40:59 -0300

    Primeiro commit

---
+++ src/main/java/com/redesocialmongo/RedesocialmongoApplication.java
@@ -0,0 +1,13 @@
+package com.redesocialmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedesocialmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedesocialmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redesocialmongo/RedesocialmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redesocialmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedesocialmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
